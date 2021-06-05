6c70ba38a9edb98b3d86d21deaa1ef22851c2049
piotrowczarczyk <powczarczyk@vp.pl>
2021-06-05 19:40:50 +0200

    added grid logic

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ window.onload = function (event) {
     const cellWidth = 20;
     const cellHeight = 20;
     const cellColor = "rgb(250,82,82)";
+    let gridWidth;
+    let gridHeight;
+    let cells;
     let readData = []
 
 
@@ -18,15 +21,30 @@ window.onload = function (event) {
         .then(data => {
                 readData = data
                     .split('\n')
-                    .map(el => el.split(' '))
-                    .filter(array => array.length > 1 || array[0] !== '')
+                    .filter(line => line !== '')
+                    .map(line => line.length > 1 ? line.split('') : line)
 
             }
         ).then(_ => main())
-    console.log(readData);
 
     function main() {
+        gridWidth = readData.shift();
+        gridHeight = readData.shift();
+        cells = readData.map(array => array.map(element => element === 'X'));
+        addBorderToCells();
+    }
 
+    function addBorderToCells() {
+        for (const row of cells) {
+            row.unshift(null);
+            row.push(null);
+        }
+        const tempRow = [];
+        for (let i = 0; i < cells[0].length; i++) {
+            tempRow.push(null);
+        }
+        cells.unshift(tempRow);
+        cells.push(tempRow);
     }
 
     function redraw() {
@@ -37,5 +55,67 @@ window.onload = function (event) {
         ctx.fillStyle = cellColor;
         ctx.fill();
         requestAnimationFrame(redraw);
+    }
+
+    function getCellNextState(cellsWithNeighbours) {
+        let aliveNeighbours = 0;
+        for (let [row, i] of cellsWithNeighbours) {
+            for (let [cell, j] of row) {
+                if ((i === 1 && j === 1) && cell) {
+                    aliveNeighbours++;
+                }
+            }
+        }
+        return cellsWithNeighbours[1][1] ? [2, 3].includes(aliveNeighbours) : aliveNeighbours === 3;
+    }
+
+    function getMiniGrid(index) {
+        const grid = [];
+
+        for (let i = 0; i < gridHeight / 2 + 2; i++) {
+            const startY = [0, 1].includes(index) ? 0 : gridHeight / 2;
+            const row = [];
+            for (let j = 0; j < gridWidth / 2 + 2; j++) {
+                const startX = [0, 2].includes(index) ? 0 : gridWidth / 2;
+                row.push(cells[startY + i][startX + j]);
+            }
+            grid.push(row);
+        }
+        return grid;
+
+        //
+        // switch (index) {
+        //     case 0:
+        //         for (let i = 0; i < grid[0].length; i++) {
+        //             grid[0][i] = false;
+        //         }
+        //         for (let i = 0; i < grid.length; i++) {
+        //             grid[i][0] = false;
+        //         }
+        //         break;
+        //     case 1:
+        //         for (let i = 0; i < grid[0].length; i++) {
+        //             grid[0][i] = false;
+        //         }
+        //         for (let i = 0; i < grid.length; i++) {
+        //             grid[i][grid[0].length - 1] = false;
+        //         }
+        //         break;
+        //     case 2:
+        //         for (let i = 0; i < grid[0].length; i++) {
+        //             grid[0][i] = false;
+        //         }
+        //         for (let i = 0; i < grid.length; i++) {
+        //             grid[i][0] = false;
+        //         }
+        //         break;
+        //     case 3:
+        //         for (let i = 0; i < grid[0].length; i++) {
+        //             grid[0][i] = false;
+        //         }
+        //         for (let i = 0; i < grid.length; i++) {
+        //             grid[i][0] = false;
+        //         }
+        // }
     }
 }
